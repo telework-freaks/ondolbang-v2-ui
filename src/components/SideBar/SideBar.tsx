a033739875750a721a8fec4bd1a1d0dadd2eeb64
rudy3091 <grayblack313@gmail.com>
2021-05-03 02:19:33 +0900

    Add sidebar toggle
    toggler를 클릭해 sidebar를 왼쪽으로 밀어넣을 수 있도록 함

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -6,6 +6,7 @@ import { SearchResultIndicator } from "@components/SideBar/SearchResultIndicator
 
 type SideBarProps = {
 	navHeight: string;
+	sidebar: "hidden" | "shown";
 };
 
 export const SideBar: React.FC<SideBarProps> = (props: SideBarProps) => {
@@ -30,23 +31,50 @@ export const SideBar: React.FC<SideBarProps> = (props: SideBarProps) => {
 		JSON.parse(JSON.stringify(dataMono)),
 	];
 
+	const [isHidden, setHidden] = React.useState<boolean>(
+		props.sidebar === "hidden"
+	);
+	const [isTogglerHidden, setTogglerHidden] = React.useState<boolean>(
+		props.sidebar === "hidden"
+	);
+
+	const ref = React.useRef<HTMLDivElement>(null);
+	const togglerRef = React.useRef<HTMLDivElement>(null);
+	const toggle = (e: any) => {
+		setHidden(!isHidden);
+		setTogglerHidden(!isTogglerHidden);
+	};
+
+	let containerClassNames = $.container;
+	if (isHidden) containerClassNames += ` ${$.hidden}`;
+
+	let togglerClassNames = $.toggler_container;
+	if (isTogglerHidden) togglerClassNames += ` ${$.toggler_hidden}`;
+
 	return (
-		<div
-			className={$.container}
-			style={{
-				height: `calc(100% - ${props.navHeight})`,
-				top: props.navHeight,
-			}}
-		>
-			<div className={$.result_indicator}>
-				<SearchResultIndicator num={dataPoly.length}></SearchResultIndicator>
+		<>
+			<div
+				ref={ref}
+				className={containerClassNames}
+				style={{
+					height: `calc(100% - ${props.navHeight})`,
+					top: props.navHeight,
+				}}
+			>
+				<div className={$.result_indicator}>
+					<SearchResultIndicator num={dataPoly.length}></SearchResultIndicator>
+				</div>
+
+				{dataPoly.map((data, i) => (
+					<div key={i} className={$.result_wrapper}>
+						<SearchResultItem {...data}></SearchResultItem>
+					</div>
+				))}
 			</div>
 
-			{dataPoly.map((data, i) => (
-				<div key={i} className={$.result_wrapper}>
-					<SearchResultItem {...data}></SearchResultItem>
-				</div>
-			))}
-		</div>
+			<div onClick={toggle} className={togglerClassNames}>
+				<div className={$.triangle}></div>
+			</div>
+		</>
 	);
 };
