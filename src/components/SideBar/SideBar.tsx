bae2775f081c016371e620e72c7aded5f8d69c9c
rudy3091 <grayblack313@gmail.com>
2021-05-02 23:53:40 +0900

    Add SideBar component
    핵심 데이터 없이 컴포넌트를 구성함
    검색결과 인디케이터와 페이지네이터는 아직 구현하지 않음
    테스트용으로 무의미한 데이터 조금을 써놓음

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,18 +1,49 @@
 import * as React from "react";
 import $ from "./SideBar.module.scss";
 
+import { SearchResultItem } from "@components/SideBar/SearchResultItem/SearchResultItem";
+
 type SideBarProps = {
 	navHeight: string;
 };
 
 export const SideBar: React.FC<SideBarProps> = (props: SideBarProps) => {
+	const testName = "테스트 검색결과";
+	const dataPairs: [number, number][] = [
+		[123456, 123967],
+		[123456, 123967],
+		[123456, 123967],
+		[123456, 123967],
+	];
+	const dataMono = {
+		name: testName,
+		data: dataPairs,
+	};
+
+	const dataPoly = [
+		JSON.parse(JSON.stringify(dataMono)),
+		JSON.parse(JSON.stringify(dataMono)),
+		JSON.parse(JSON.stringify(dataMono)),
+		JSON.parse(JSON.stringify(dataMono)),
+		JSON.parse(JSON.stringify(dataMono)),
+		JSON.parse(JSON.stringify(dataMono)),
+	];
+
 	return (
 		<div
 			className={$.container}
-			style={{ height: `calc(100% - ${props.navHeight})`, top: props.navHeight }}
+			style={{
+				height: `calc(100% - ${props.navHeight})`,
+				top: props.navHeight,
+			}}
 		>
-			<div className={$.result_summary}>
-			</div>
+			<div className={$.result_summary}></div>
+
+				{dataPoly.map((data, i) => (
+					<div className={$.result_wrapper}>
+						<SearchResultItem key={i} {...data}></SearchResultItem>
+					</div>
+				))}
 		</div>
 	);
 };
