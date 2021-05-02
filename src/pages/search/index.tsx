1f84124373438572e390206ef9b912f47abd5ada
rudy3091 <grayblack313@gmail.com>
2021-05-02 17:15:05 +0900

    Add map context to /search page
    MapInitializer 컴포넌트를 이용해 화면에 지도를 보여줌
    카카오맵api 로드를 위한 스크립트 태그를 컴포넌트로 분리해 재사용하는게 좋을것같음
    (지금은 next/head 아래에 하드코딩해놓음)

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,3 +1,26 @@
+import Head from "next/head";
+import Link from "next/link";
+import dynamic from "next/dynamic";
+
+import { NavBar } from "@components/NavBar/NavBar";
+import MapInitializer from "@components/MapInitializer/MapInitializer";
+
+// const MapInitializer = dynamic(
+// 	() => import("@components/MapInitializer/MapInitializer"),
+// 	{ loading: () => <div>loading maps...</div>, ssr: false }
+// );
+
 export default function Search() {
-	return <h1>Hello search page</h1>
+	return (
+		<>
+			<Head>
+				<script
+					type="text/javascript"
+					src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APIKEY}&libraries=services,clusterer,drawing`}
+				></script>
+			</Head>
+			<NavBar></NavBar>
+			<MapInitializer></MapInitializer>
+		</>
+	);
 }
